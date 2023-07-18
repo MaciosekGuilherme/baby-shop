ee2b91d8b132127cffd3637e7c77d062b294047a
MaciosekGuilherme <guilherme.maciosek@hotmail.com>
2023-07-18 00:14:07 -0300

    primeiro push

---
+++ backend/src/main/java/com/store/marketbackend/MarketbackendApplication.java
@@ -0,0 +1,13 @@
+package com.store.marketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbackendApplication.class, args);
+	}
+
+}
