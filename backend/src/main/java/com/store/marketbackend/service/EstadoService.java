f56a23f00368869459d0b04d8be76614aeb9530c
MaciosekGuilherme <guilherme.maciosek@hotmail.com>
2023-07-19 23:43:42 -0300

    crud estado criado

---
+++ backend/src/main/java/com/store/marketbackend/service/EstadoService.java
@@ -0,0 +1,40 @@
+package com.store.marketbackend.service;
+
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.store.marketbackend.entity.Estado;
+import com.store.marketbackend.repository.EstadoRepository;
+
+@Service
+public class EstadoService {
+    @Autowired
+
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> buscarTodos() {
+        return estadoRepository.findAll();
+    }
+
+    public Estado inserir(Estado estado) {
+        estado.setDataCriacao(Timestamp.valueOf(LocalDateTime.now()));
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public Estado alterar(Estado estado) {
+        estado.setDataAtualizacao(Timestamp.valueOf(LocalDateTime.now()));
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public void excluir(Long id) {
+        Estado estado = estadoRepository.findById(id).get();
+        estadoRepository.delete(estado);
+    }
+
+}
